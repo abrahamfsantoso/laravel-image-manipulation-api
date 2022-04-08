73784cb86e1e1fdb61a0716196bdfc0c43dccdbe
Abraham F. Santoso <abrahamfsantoso@gmail.com>
2022-04-08 20:13:08 +0700

    resize function and authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::get('/token/create', [DashboardController::class, 'showTokenForm'])->name('token.showForm');
+    Route::post('/token/create', [DashboardController::class, 'createToken'])->name('token.create');
+    Route::post('/token/delete/{token}', [DashboardController::class, 'deleteToken'])->name('token.delete');
+});
+
+require __DIR__ . '/auth.php';
